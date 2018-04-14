014c0ddb95a7ac7ab3d92322749ff6e3b6a9c63b
hello57748 <sampeters@csumb.edu>
2018-04-14 15:29:23 -0700

    layoutFunctions is new name of folder

---
+++ layoutFunctions/dragAndDropDisplay.py
@@ -0,0 +1,29 @@
+#*******************************************************************************
+# Contributors: Samuel Peters
+# Last Changed: 14 April 2018
+# Description: This file will create a layout that has drag and drop functionality to add images to the photo editor. This file will also contain the layout to display images that are added through drag and drop.
+#*******************************************************************************
+
+hello = "hello world!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Summary:
+# Preconditions:
+# Postconditions:
+#*******************************************************************************************************
