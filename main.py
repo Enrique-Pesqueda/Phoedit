a0d2783d9aed1c768333711c6a324649be3fdec1
hello57748 <sampeters@csumb.edu>
2018-04-14 14:43:50 -0700

    main layout setup

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QVBoxLayout, QPushButton, QComboBox, QLabel
+from PIL import Image
+from PyQt5.QtGui import QIcon, QPixmap
 
-class Example(QWidget):
+class MainPage(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("PhoeEdit")
 
+        #MAIN LAYOUT
+        mbox = QVBoxLayout()
+        self.setLayout(mbox)
 
-
-
-
-
-
-
+        #SHOW EVERYTHING
         self.show()
 
-
 app = QApplication(sys.argv)
-ex = Example()
-sys.exit(app.exec_())
+page = MainPage()
+status = app.exec_()
+sys.exit(status)
