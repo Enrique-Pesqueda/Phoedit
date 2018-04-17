2a755146dc9f20a6c60cb3679f7a17d176fd1674
hello57748 <sampeters@csumb.edu>
2018-04-16 18:32:22 -0700

    photo display testing

--- main.py
+++ main.py
@@ -4,18 +4,39 @@
 # Description: This is the main file that brings all of the pyqt layouts together to create the completed GUI.
 #*******************************************************************************************************
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QVBoxLayout, QPushButton, QComboBox, QLabel
+import dragAndDropImageFunctions as ddif
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QLineEdit, QVBoxLayout, QPushButton, QComboBox, QLabel
 from PIL import Image
 from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import Qt
 
 class MainPage(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("PhoeEdit")
+        self.setWindowTitle("phoEDIT")
+        self.picToEdit='IMG_0321.png'
+
+        #DRAG AND DROP / IMAGE DISPLAY LAYOUT
+        displayPicText = QLabel(self.picToEdit)
+        displayPicHolder = QLabel(self)
+        displayPic = QPixmap(self.picToEdit)
+        displayPic = displayPic.scaled(300, 300, Qt.KeepAspectRatio)
+        displayPicHolder.setPixmap(displayPic)
+        vbox1 = QVBoxLayout()
+        vbox1.addWidget(displayPicText)
+        vbox1.addWidget(displayPicHolder)
 
         #MAIN LAYOUT
         mbox = QVBoxLayout()
+        mbox.addLayout(vbox1)
         self.setLayout(mbox)
+        self.setGeometry(0, 0, 800, 600)
+            # Set window background color
+        self.setAutoFillBackground(True)
+        p = self.palette()
+        p.setColor(self.backgroundRole(), Qt.darkGray)
+        self.setPalette(p)
+
 
         #SHOW EVERYTHING
         self.show()
