b2a21835d875b5cb5334fc6cc8e6838e40ef3cb0
sbstn540 <sruiz5260@gmail.com>
2018-04-18 15:16:54 -0700

    merge error fix

--- main.py
+++ main.py
@@ -9,79 +9,90 @@ from PyQt5.QtWidgets import *
 from PIL import Image
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+import os
 
 class MainPage(QWidget):
+
     #*******************************************************************************************************
-    # Summary: This function initializes the Main window which will contain the image being edited and all
-    #          possible editing options for that image.
     def __init__(self):
         super().__init__()
-        #WINDOW SETTINGS
         self.setWindowTitle("phoEDIT")
+        self.picToEdit=''
+
+        #FILE EXPLORER / IMAGE DISPLAY LAYOUT
+        self.picToEdit = FileExplorer.openFileNameDialog(self)
+        displayPicText = QLabel(self.picToEdit)
+        displayPicHolder = QLabel(self)
+        displayPic = QPixmap(self.picToEdit)
+        displayPic = displayPic.scaled(300, 300, Qt.KeepAspectRatio)
+        displayPicHolder.setPixmap(displayPic)
+        vbox1 = QVBoxLayout()
+        vbox1.addWidget(displayPicText)
+        vbox1.addWidget(displayPicHolder)
+
+        #MAIN LAYOUT
+        mbox = QVBoxLayout()
+        mbox.addLayout(vbox1)
+        self.setLayout(mbox)
+        self.setGeometry(0, 0, 800, 600)
+
+        #RGB sliders
+        vbox2 = QVBoxLayout()
+        self.sl = QSlider(Qt.Horizontal, self)
+        self.sl.setMinimum(-50)
+        self.sl.setMaximum(50)
+        self.sl.setValue(0)
+        self.sl.setTickPosition(QSlider.TicksBelow)
+        self.sl.setTickInterval(5)
+        self.s2 = QSlider(Qt.Horizontal, self)
+        self.s2.setMinimum(-50)
+        self.s2.setMaximum(50)
+        self.s2.setValue(0)
+        self.s2.setTickPosition(QSlider.TicksBelow)
+        self.s2.setTickInterval(5)
+        self.s3 = QSlider(Qt.Horizontal, self)
+        self.s3.setMinimum(-50)
+        self.s3.setMaximum(50)
+        self.s3.setValue(0)
+        self.s3.setTickPosition(QSlider.TicksBelow)
+        self.s3.setTickInterval(5)
+        vbox2.addWidget(self.s1)
+        vbox2.addWidget(self.s2)
+        vbox2.addWidget(self.s3)
+        self.sl.valueChanged.connect(self.redValueChange)
+        self.s2.valueChanged.connect(self.greenValueChange)
+        self.s3.valueChanged.connect(self.blueValueChange)
+
+        #SET WINDOW BACKGROUND COLOR
         self.setAutoFillBackground(True)
         p = self.palette()
         p.setColor(self.backgroundRole(), Qt.darkGray)
         self.setPalette(p)
 
-        #IMPORTANT VARIABLES
-        self.picToEdit=''
-        self.OriginalPic = 'noImageSelected.jpg'
-
-        #FILE EXPLORER / IMAGE DISPLAY LAYOUT
-        self.displayPicText = QLabel(self.picToEdit)
-        self.displayPicHolder = QLabel(self)
-        self.displayPic = QPixmap(self.picToEdit)
-        self.displayPic = self.displayPic.scaled(300, 300, Qt.KeepAspectRatio)
-        self.displayPicHolder.setPixmap(self.displayPic)
-        self.fileExplorerButton = QPushButton('Choose Image to Edit')
-        self.vbox1 = QVBoxLayout()
-        self.vbox1.addWidget(self.fileExplorerButton)
-        self.vbox1.addWidget(self.displayPicText)
-        self.vbox1.addWidget(self.displayPicHolder)
-
-        #MAIN LAYOUT
-        self.mbox = QVBoxLayout()
-        self.mbox.addLayout(self.vbox1)
-        self.setLayout(self.mbox)
-        self.setGeometry(0, 0, 800, 600)
-
-        #FUNCTION SLOTS
-        self.fileExplorerButton.clicked.connect(self.openFileExplorerWindow)
-
         #SHOW EVERYTHING
         self.show()
+    # Summary: This function initializes the Main window which will contain the image being edited and all possible editing options for that image.
+    # Preconditions: N/A
+    # Postconditions: The main window will be shown on the desktop.
     #*******************************************************************************************************
-    # Summary: This function opens the file explorer to select an image to be edited. This changes the
-    #          current photo being displayed and the file path text that is displayed to the corresponding image
-    #          file.
-    @pyqtSlot()
-    def openFileExplorerWindow(self):
-        self.picToEdit = FileExplorer.openFileNameDialog(self)
-        self.updateMainPage()
-    #*******************************************************************************************************
-    # Summary: This function updates the main display by deleting widgets adding new widgets with updated
-    #          information to the mainpage.
-    def updateMainPage(self):
-        #updates picture display and file path text display
-        self.vbox1.removeWidget(self.displayPicText)
-        self.displayPicText.deleteLater()
-        self.vbox1.removeWidget(self.displayPicHolder)
-        self.displayPicHolder.deleteLater()
-        self.displayPicText = QLabel(self.picToEdit)
-        self.displayPicHolder = QLabel(self)
-        self.displayPic = QPixmap(self.picToEdit)
-        self.displayPic = self.displayPic.scaled(300, 300, Qt.KeepAspectRatio)
-        self.displayPicHolder.setPixmap(self.displayPic)
-        self.vbox1.addWidget(self.displayPicText)
-        self.vbox1.addWidget(self.displayPicHolder)
-    #*******************************************************************************************************
-
-
+    @pyqtSlot
+    def redValueChange(self):
+        value = self.sl.value()
+        im = Image.open(self.picToEdit)
+    @pyqtSlot
+    def greenValueChange(self):
+        value = self.s2.value()
+        im = Image.open(self.picToEdit)
+    @pyqtSlot
+    def blueValueChange(self):
+        value = self.s3.value()
+        im = Image.open(self.picToEdit)
     #PUT THIS WHEN WRITING NEW FUNCTIONS
     #*******************************************************************************************************
-    # Summary:
     # Function
+    # Summary:
     #*******************************************************************************************************
+
 
 app = QApplication(sys.argv)
 page = MainPage()
