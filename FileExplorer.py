f9b59b987c25fd8545ac0df72749b851a22725e7
hello57748 <sampeters@csumb.edu>
2018-04-18 14:28:40 -0700

    file explorer displays image and changes text

--- FileExplorer.py
+++ FileExplorer.py
@@ -10,6 +10,9 @@ from PyQt5.QtGui import *
 class FileExplorer(QWidget):
 
     #*******************************************************************************************************
+    # Summary: This function displays a file explorer window that can be used to choose an image file to edit.
+    # Preconditions: N/A
+    # Postconditions: A string containing the full path to the chosen image file will be returned.
     def openFileNameDialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
@@ -17,7 +20,4 @@ class FileExplorer(QWidget):
         if fileName:
             return fileName
             print(fileName)
-    # Summary: This function displays a file explorer window that can be used to choose an image file to edit.
-    # Preconditions: N/A
-    # Postconditions: A string containing the full path to the chosen image file will be returned.
     #*******************************************************************************************************
--- main.py
+++ main.py
@@ -11,52 +11,77 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
 class MainPage(QWidget):
-
     #*******************************************************************************************************
+    # Summary: This function initializes the Main window which will contain the image being edited and all
+    #          possible editing options for that image.
     def __init__(self):
         super().__init__()
+        #WINDOW SETTINGS
         self.setWindowTitle("phoEDIT")
-        self.picToEdit=''
-
-        #FILE EXPLORER / IMAGE DISPLAY LAYOUT
-        self.picToEdit = FileExplorer.openFileNameDialog(self)
-        displayPicText = QLabel(self.picToEdit)
-        displayPicHolder = QLabel(self)
-        displayPic = QPixmap(self.picToEdit)
-        displayPic = displayPic.scaled(300, 300, Qt.KeepAspectRatio)
-        displayPicHolder.setPixmap(displayPic)
-        vbox1 = QVBoxLayout()
-        vbox1.addWidget(displayPicText)
-        vbox1.addWidget(displayPicHolder)
-
-        #MAIN LAYOUT
-        mbox = QVBoxLayout()
-        mbox.addLayout(vbox1)
-        self.setLayout(mbox)
-        self.setGeometry(0, 0, 800, 600)
-
-        #SET WINDOW BACKGROUND COLOR
         self.setAutoFillBackground(True)
         p = self.palette()
         p.setColor(self.backgroundRole(), Qt.darkGray)
         self.setPalette(p)
 
+        #IMPORTANT VARIABLES
+        self.picToEdit='noImageSelected.jpg'
+
+
+        #FILE EXPLORER / IMAGE DISPLAY LAYOUT
+        self.displayPicText = QLabel(self.picToEdit)
+        self.displayPicHolder = QLabel(self)
+        self.displayPic = QPixmap(self.picToEdit)
+        self.displayPic = self.displayPic.scaled(300, 300, Qt.KeepAspectRatio)
+        self.displayPicHolder.setPixmap(self.displayPic)
+        self.fileExplorerButton = QPushButton('Choose Image to Edit')
+        self.vbox1 = QVBoxLayout()
+        self.vbox1.addWidget(self.fileExplorerButton)
+        self.vbox1.addWidget(self.displayPicText)
+        self.vbox1.addWidget(self.displayPicHolder)
+
+        #MAIN LAYOUT
+        self.mbox = QVBoxLayout()
+        self.mbox.addLayout(self.vbox1)
+        self.setLayout(self.mbox)
+        self.setGeometry(0, 0, 800, 600)
+
+        #FUNCTION SLOTS
+        self.fileExplorerButton.clicked.connect(self.openFileExplorerWindow)
+
         #SHOW EVERYTHING
         self.show()
-    # Summary: This function initializes the Main window which will contain the image being edited and all possible editing options for that image.
-    # Preconditions: N/A
-    # Postconditions: The main window will be shown on the desktop.
+    #*******************************************************************************************************
+    # Summary: This function opens the file explorer to select an image to be edited. This changes the
+    #          current photo being displayed and the file path text that is displayed to the corresponding image
+    #          file.
+    @pyqtSlot()
+    def openFileExplorerWindow(self):
+        self.picToEdit = FileExplorer.openFileNameDialog(self)
+        self.updateMainPage()
+    #*******************************************************************************************************
+    # Summary: This function updates the main display by deleting widgets adding new widgets with updated
+    #          information to the mainpage.
+    def updateMainPage(self):
+        #updates picture display and file path text display
+        self.vbox1.removeWidget(self.displayPicText)
+        self.displayPicText.deleteLater()
+        self.vbox1.removeWidget(self.displayPicHolder)
+        self.displayPicHolder.deleteLater()
+        self.displayPicText = QLabel(self.picToEdit)
+        self.displayPicHolder = QLabel(self)
+        self.displayPic = QPixmap(self.picToEdit)
+        self.displayPic = self.displayPic.scaled(300, 300, Qt.KeepAspectRatio)
+        self.displayPicHolder.setPixmap(self.displayPic)
+        self.vbox1.addWidget(self.displayPicText)
+        self.vbox1.addWidget(self.displayPicHolder)
     #*******************************************************************************************************
 
 
     #PUT THIS WHEN WRITING NEW FUNCTIONS
     #*******************************************************************************************************
-    # Function
     # Summary:
-    # Preconditions:
-    # Postconditions:
+    # Function
     #*******************************************************************************************************
-
 
 app = QApplication(sys.argv)
 page = MainPage()
