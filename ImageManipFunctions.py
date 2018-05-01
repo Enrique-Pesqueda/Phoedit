5ac5549b106c9f7be68f02bfdc48b7d306addf23
hello57748 <sampeters@csumb.edu>
2018-04-30 17:53:16 -0700

    Samuel Peters: fixed updated function, Sebastian Ruiz: Created and implemented rgb sliders that edit image

--- ImageManipFunctions.py
+++ ImageManipFunctions.py
@@ -1,50 +1,66 @@
 #*******************************************************************************************************
 # imageManipFunctions.py
 # Contributors: Sebastian Ruiz
-# Last Changed: 15 April 2018
+# Last Changed: 25 April 2018
 # Description: Saturation and Vibrance editing functions.
 #*******************************************************************************************************
-
+from PIL import Image
+class ImageManipFunctions():
+#*******************************************************************************************************
+# Summary: Return value that will be added to color chanel after slider movement.
+    def valueArithmeticAdvancedRecordKeepingFunction(value, lastRecordedValue):
+        if value == lastRecordedValue:
+            return 0
+        elif value == 0:
+            return  (-1 * lastRecordedValue)
+        else:
+            return (value - lastRecordedValue)
 #*******************************************************************************************************
 # Summary: RGB saturation functions take in a pillow image object and a value
 #          the getdata() method is used to be able to alter a specific color channel
 #          the specified color channel is modified by adding a passed in value to it
 #          this is done for every pixel in an image and are saved into a new list
-def redSaturation(image, value):
-    list = image.getdata()
-    new_list = []
-    for pixel in image:
-        pixel[0] += value
-        if pixel[0] > 255:
-            pixel[0] = 255
-        temp = (pixel[0],pixel[1],pixel[2])
-        new_list.append(temp)
-    picture.putdata(new_list)
-    picture.save('x')
+    def redSaturation(imageData, value, destination):
+        new_list = []
+        redPixel = 0
+        for pixel in imageData:
+            redPixel = pixel[0] + value
+            if redPixel > 255:
+                redPixel = 255
+            temp = (redPixel,pixel[1],pixel[2])
+            new_list.append(temp)
+        im = Image.open(destination)
+        im.putdata(new_list)
+        im.save(destination)
+        return new_list
 
-def greenSaturation(image, value):
-    list = image.getdata()
-    new_list = []
-    for pixel in image:
-        pixel[1] += value
-        if pixel[1] > 255:
-            pixel[1] = 255
-        temp = (pixel[0],pixel[1],pixel[2])
-        new_list.append(temp)
-    picture.putdata(new_list)
-    picture.save('x')
+    def greenSaturation(imageData, value, destination):
+        new_list = []
+        greenPixel = 0
+        for pixel in imageData:
+            greenPixel = pixel[1] + value
+            if greenPixel > 255:
+                greenPixel = 255
+            temp = (pixel[0],greenPixel,pixel[2])
+            new_list.append(temp)
+        im = Image.open(destination)
+        im.putdata(new_list)
+        im.save(destination)
+        return new_list
 
-def blueSaturation(image, value):
-    list = image.getdata()
-    new_list = []
-    for pixel in image:
-        pixel[2] += value
-        if pixel[2] > 255:
-            pixel[2] = 255
-        temp = (pixel[0],pixel[1],pixel[2])
-        new_list.append(temp)
-    picture.putdata(new_list)
-    picture.save('x')
+    def blueSaturation(imageData, value, destination):
+        new_list = []
+        bluePixel = 0
+        for pixel in imageData:
+            bluePixel = pixel[2] + value
+            if bluePixel > 255:
+                bluePixel = 255
+            temp = (pixel[0],pixel[1],bluePixel)
+            new_list.append(temp)
+        im = Image.open(destination)
+        im.putdata(new_list)
+        im.save(destination)
+        return new_list
 #*******************************************************************************************************
 # Summary: RGB vibrance functions take in a pillow image object and a value
 #          the getdata() method is used to be able to alter a specific color channel
@@ -53,7 +69,7 @@ def redVibrance(image, value):
     list = image.getdata()
     new_list = []
     for pixel in image:
-        if(pixel[0] > pixel[1] and pixel[0] > pixel[2]):
+        if pixel[0] > pixel[1] and pixel[0] > pixel[2]:
             pixel[0] += value
             if pixel[0] > 255:
                 pixel[0] = 255
@@ -68,7 +84,7 @@ def greenVibrance(image, value):
     list = image.getdata()
     new_list = []
     for pixel in image:
-        if(pixel[1] > pixel[0] and pixel[1] > pixel[2]):
+        if pixel[1] > pixel[0] and pixel[1] > pixel[2]:
             pixel[1] += value
             if pixel[1] > 255:
                 pixel[1] = 255
--- main.py
+++ main.py
@@ -5,12 +5,12 @@
 # Description: This is the main file that brings all of the pyqt layouts together to create the completed GUI.
 #*******************************************************************************************************
 import sys
+from FileDialog import FileDialog
+from ImageManipFunctions import ImageManipFunctions
 from PyQt5.QtWidgets import *
 from PIL import Image
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-from FileExplorer import FileExplorer
-from FilterFunctions import FilterFunctions
 
 class MainPage(QWidget):
     #*******************************************************************************************************
@@ -27,13 +27,17 @@ class MainPage(QWidget):
         self.setPalette(p)
 
         #IMPORTANT VARIABLES
-        self.picToEdit=''
-        self.OriginalPic = 'noImageSelected.jpg'
+        self.picToEditData = []
+        self.originalPic = 'images/noImageSelected.jpg'
+        self.destination = ''
+        self.redSaturationValue = 0
+        self.greenSaturationValue = 0
+        self.blueSaturationValue = 0
 
         #FILE EXPLORER / IMAGE DISPLAY LAYOUT
-        self.displayPicText = QLabel(self.picToEdit)
+        self.displayPicText = QLabel(self.originalPic)
         self.displayPicHolder = QLabel(self)
-        self.displayPic = QPixmap(self.picToEdit)
+        self.displayPic = QPixmap(self.originalPic)
         self.displayPic = self.displayPic.scaled(300, 300, Qt.KeepAspectRatio)
         self.displayPicHolder.setPixmap(self.displayPic)
         self.fileExplorerButton = QPushButton('Choose Image to Edit')
@@ -42,20 +46,42 @@ class MainPage(QWidget):
         self.vbox1.addWidget(self.displayPicText)
         self.vbox1.addWidget(self.displayPicHolder)
 
-        #FILTER FUNCTIONS LAYOUT
+        #IMAGE MANIPULATION SLIDERS LAYOUT
+        self.vbox2 = QVBoxLayout()
+        self.s1 = QSlider(Qt.Horizontal, self)
+        self.s1.setMinimum(-50)
+        self.s1.setMaximum(50)
+        self.s1.setValue(0)
+        self.s1.setTickPosition(QSlider.TicksBelow)
+        self.s1.setTickInterval(5)
+        self.s2 = QSlider(Qt.Horizontal, self)
+        self.s2.setMinimum(-50)
+        self.s2.setMaximum(50)
+        self.s2.setValue(0)
+        self.s2.setTickPosition(QSlider.TicksBelow)
+        self.s2.setTickInterval(5)
+        self.s3 = QSlider(Qt.Horizontal, self)
+        self.s3.setMinimum(-50)
+        self.s3.setMaximum(50)
+        self.s3.setValue(0)
+        self.s3.setTickPosition(QSlider.TicksBelow)
+        self.s3.setTickInterval(5)
+        self.vbox2.addWidget(self.s1)
+        self.vbox2.addWidget(self.s2)
+        self.vbox2.addWidget(self.s3)
 
         #MAIN LAYOUT
         self.mbox = QVBoxLayout()
-        self.hbox1 = QHBoxLayout()
-        self.hbox2 = QHBoxLayout()
-        self.hbox1.addLayout(self.vbox1)
-        self.mbox.addLayout(self.hbox1)
-        self.mbox.addLayout(self.hbox2)
+        self.mbox.addLayout(self.vbox1)
+        self.mbox.addLayout(self.vbox2)
         self.setLayout(self.mbox)
         self.setGeometry(0, 0, 800, 600)
 
         #FUNCTION SLOTS
         self.fileExplorerButton.clicked.connect(self.openFileExplorerWindow)
+        self.s1.valueChanged.connect(self.redValueChange)
+        self.s2.valueChanged.connect(self.greenValueChange)
+        self.s3.valueChanged.connect(self.blueValueChange)
 
         #SHOW EVERYTHING
         self.show()
@@ -65,32 +91,104 @@ class MainPage(QWidget):
     #          file.
     @pyqtSlot()
     def openFileExplorerWindow(self):
-        self.picToEdit = FileExplorer.openFileNameDialog(self)
+        self.originalPic = FileDialog.openFileNameDialog(self)
+        self.destination = 'images/001' + FileDialog.fileExtensionGrabber(self.originalPic)
+        im = Image.open(self.originalPic)
+        im.save(self.destination)
+        self.picToEditData = im.getdata()
+        self.redSaturationValue = 0
+        self.greenSaturationValue = 0
+        self.blueSaturationValue = 0
         self.updateMainPage()
     #*******************************************************************************************************
     # Summary: This function updates the main display by deleting widgets adding new widgets with updated
     #          information to the mainpage.
     def updateMainPage(self):
 
-        #UPDATES PICTURE DISPLAY AND FILE PATH TEXT DISPLAY
+        #DELETES EVERYTHING IN GUI
+            #file explore / image display layout
         self.vbox1.removeWidget(self.displayPicText)
         self.displayPicText.deleteLater()
         self.vbox1.removeWidget(self.displayPicHolder)
         self.displayPicHolder.deleteLater()
-        self.displayPicText = QLabel(self.picToEdit)
+            #image manipulation sliders layout
+        self.vbox2.removeWidget(self.s1)
+        self.s1.deleteLater()
+        self.vbox2.removeWidget(self.s2)
+        self.s2.deleteLater()
+        self.vbox2.removeWidget(self.s3)
+        self.s3.deleteLater()
+
+        #CREATES NEW GUI
+            #adds picture diplay and picture path text
+        self.displayPicText = QLabel(self.originalPic)
         self.displayPicHolder = QLabel(self)
-        self.displayPic = QPixmap(self.picToEdit)
+        self.displayPic = QPixmap(self.destination)
         self.displayPic = self.displayPic.scaled(300, 300, Qt.KeepAspectRatio)
         self.displayPicHolder.setPixmap(self.displayPic)
         self.vbox1.addWidget(self.displayPicText)
         self.vbox1.addWidget(self.displayPicHolder)
+            #adds new rgb sliders
+        self.s1 = QSlider(Qt.Horizontal, self)
+        self.s1.setMinimum(-50)
+        self.s1.setMaximum(50)
+        self.s1.setValue(self.redSaturationValue)
+        self.s1.setTickPosition(QSlider.TicksBelow)
+        self.s1.setTickInterval(5)
+        self.s2 = QSlider(Qt.Horizontal, self)
+        self.s2.setMinimum(-50)
+        self.s2.setMaximum(50)
+        self.s2.setValue(self.greenSaturationValue)
+        self.s2.setTickPosition(QSlider.TicksBelow)
+        self.s2.setTickInterval(5)
+        self.s3 = QSlider(Qt.Horizontal, self)
+        self.s3.setMinimum(-50)
+        self.s3.setMaximum(50)
+        self.s3.setValue(self.blueSaturationValue)
+        self.s3.setTickPosition(QSlider.TicksBelow)
+        self.s3.setTickInterval(5)
+        self.vbox2.addWidget(self.s1)
+        self.vbox2.addWidget(self.s2)
+        self.vbox2.addWidget(self.s3)
+        self.s1.valueChanged.connect(self.redValueChange)
+        self.s2.valueChanged.connect(self.greenValueChange)
+        self.s3.valueChanged.connect(self.blueValueChange)
+    #*******************************************************************************************************
+    # Summary: Links to RGB saturation sliders and calls image manipulation functions  from
+    #          ImageManipFunctions class.
+    @pyqtSlot()
+    def redValueChange(self):
+        value = self.s1.value()
+        temp = value
+        value = ImageManipFunctions.valueArithmeticAdvancedRecordKeepingFunction(value, self.redSaturationValue)
+        self.picToEditData = ImageManipFunctions.redSaturation(self.picToEditData, value, self.destination)
+        self.redSaturationValue = temp
+        self.updateMainPage()
+
+    @pyqtSlot()
+    def greenValueChange(self):
+        value = self.s2.value()
+        temp = value
+        value = ImageManipFunctions.valueArithmeticAdvancedRecordKeepingFunction(value, self.greenSaturationValue)
+        self.picToEditData = ImageManipFunctions.greenSaturation(self.picToEditData, value, self.destination)
+        self.greenSaturationValue = temp
+        self.updateMainPage()
+
+    @pyqtSlot()
+    def blueValueChange(self):
+        value = self.s3.value()
+        temp = value
+        value = ImageManipFunctions.valueArithmeticAdvancedRecordKeepingFunction(value, self.blueSaturationValue)
+        self.picToEditData = ImageManipFunctions.blueSaturation(self.picToEditData, value, self.destination)
+        self.blueSaturationValue = temp
+        self.updateMainPage()
     #*******************************************************************************************************
 
 
+    #PUT THIS WHEN WRITING NEW FUNCTIONS
     #*******************************************************************************************************
-    # Summary: This function will apply the Gray filter to picToEdit.
-    # def applyGrayFilter(self):
-
+    # Summary:
+    # Function
     #*******************************************************************************************************
 
 app = QApplication(sys.argv)
